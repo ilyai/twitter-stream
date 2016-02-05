e5b3ce533b0d21ff104a2ce33d1fd8b6eeeb1297
Ilya Igonkin <osdev0@gmail.com>
2016-02-05 17:13:52 +0400

    2.2.3 Asynchronously transforming the twitter stream

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,19 +1,32 @@
 package controllers
 
+import
+java.nio.charset.Charset
+
 import play.api._
-import play.api.libs.iteratee.Iteratee
+import play.api.libs.iteratee.{Enumerator, Concurrent, Iteratee}
+import play.api.libs.json.JsObject
 import play.api.libs.oauth.{OAuthCalculator, RequestToken, ConsumerKey}
 import play.api.libs.ws._
 import play.api.mvc._
 import play.api.Play.current
 import scala.concurrent.ExecutionContext.Implicits.global
-
+import play.api.libs.iteratee._
+import play.api.libs.json._
+import play.extras.iteratees._
 import scala.concurrent.Future
 
 class Application extends Controller {
 
-  val loggingIteratee = Iteratee.foreach[Array[Byte]] { array =>
-    Logger.info(array.map(_.toChar).mkString)
+  val (iteratee, enumerator) = Concurrent.joined[Array[Byte]]
+
+  val jsonStream: Enumerator[JsObject] =
+      enumerator &>
+      Encoding.decode() &>
+      Enumeratee.grouped(JsonIteratees.jsSimpleObject)
+
+  val loggingIteratee = Iteratee.foreach[JsObject] { value =>
+    Logger.info(value.toString())
   }
 
   def index = Action {
@@ -30,7 +43,7 @@ class Application extends Controller {
         ConsumerKey(apiKey, apiSecret),
         RequestToken(token, tokenSecret)
         )
-
+    jsonStream.run(loggingIteratee)
     credentials.map {
       case (consumerKey, requestToken) =>
         WS.url("https://stream.twitter.com/1.1/statuses/filter.json")
@@ -38,7 +51,7 @@ class Application extends Controller {
           .withQueryString("track" -> "reactive")
           .get { response =>
             Logger.info("Status: " + response.status)
-            loggingIteratee
+            iteratee
           }.map { _ =>
             Ok("Stream closed")
           }
